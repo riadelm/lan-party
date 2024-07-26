636afac8b309d4eb7f8e7d8ae547b3b636f9e872
riadelm <elmahmoudy.riad@gmail.com>
2024-07-26 02:35:43 -0400

    feat: working half loading screen

--- lan-party/src/homepage.js
+++ lan-party/src/homepage.js
@@ -5,12 +5,18 @@ import images from './utils/importAllHomepageImages';
 import clickImage from './assets/homepage/buttons/click.png'
 import forumImage from './assets/homepage/buttons/forum.png'
 import logoGif from './assets/homepage/buttons/logo.gif'
-import loading1 from './assets/homepage/loading/loading1.gif';
+import loading1 from './assets/homepage/loading/loadingbar1.gif';
+import loading2 from './assets/homepage/loading/loadingbar2.gif';
+import loading3 from './assets/homepage/loading/loadingbar3.gif';
+import loading4 from './assets/homepage/loading/loadingbarfinal.gif';
 
 function HomePage() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [pixelSize, setPixelSize] = useState(0);
-  const [showLoading, setShowLoading] = useState(false);
+  const [showLoading1, setShowLoading1] = useState(false);
+  const [showLoading2, setShowLoading2] = useState(false);
+  const [showLoading3, setShowLoading3] = useState(false);
+  const [showLoading4, setShowLoading4] = useState(false);
   const [rotateImages, setRotateImages] = useState(true);
   const containerRef = useRef(null);
   const pixelatedImageRef = useRef(null);
@@ -69,14 +75,27 @@ function HomePage() {
     setRotateImages(false);
     setPixelSize(50);
     setTimeout(() => {
-      setShowLoading(true);
+      setShowLoading1(true);
       setTimeout(() => {
-        setShowLoading(false);
-        setRotateImages(true);
-      }, 3000); // Show loading for 3 seconds
+        setShowLoading2(true);
+        setTimeout(() => {
+          setShowLoading3(true);
+          setShowLoading1(false);
+          setTimeout(() => {
+            setShowLoading2(false);
+            setTimeout(() => {
+                setShowLoading3(false);
+                setShowLoading4(true);
+                setTimeout(() => {
+                    setShowLoading4(false);
+                    setRotateImages(true);
+                }, 5000); // show full loading image for 5 seconds
+            }, 3000); // show third for 3 more seconds while removing second loading gif
+          }, 7000); // Show third loading gif for 7 seconds
+        }, 6000); // Show second loading gif for 6 seconds
+      }, 3000); // Show first loading gif for 3 seconds
     }, 2000); // Keep pixelSize 50 for 2 seconds
   };
-
   return (
     <div className="background">
       <ImagePixelated
@@ -88,7 +107,7 @@ function HomePage() {
         fillTransparencyColor={"grey"}
         style={{ objectFit: 'cover', display: 'block' }}
       />
-      {!showLoading && (
+      {!showLoading1 && !showLoading2 && !showLoading3 && !showLoading4 &&(
         <img
           src={clickImage}
           alt="Click Button"
@@ -96,11 +115,32 @@ function HomePage() {
           onClick={handleClick}
         />
       )}
-      {showLoading && (
+      {showLoading1 && (
         <img
           src={loading1}
-          alt="Loading"
-          className="loading-gif"
+          alt="Loading 1"
+          className="loading-gif-1"
+        />
+      )}
+      {showLoading2 && (
+        <img
+          src={loading2}
+          alt="Loading 2"
+          className="loading-gif-2"
+        />
+      )}
+      {showLoading3 && (
+        <img
+          src={loading3}
+          alt="Loading 3"
+          className="loading-gif-3"
+        />
+      )}
+      {showLoading4 && (
+        <img
+          src={loading4}
+          alt="Loading 4"
+          className="loading-gif-4"
         />
       )}
       <img
